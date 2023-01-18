f4655acadc27e74bf54ba29dae250a1951bc8a87
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2023-01-17 22:42:38 -0500

    Create template map

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # Select multiple points on a map and find the average distance between any two points
 # Then generate a map showing the midpoint and the distances between each point
 
+import folium
+import geopy.distance
+
+bMap = folium.Map(location=[39.8283,-98.5795], zoom_start=5)
+folium.TileLayer('stamenwatercolor').add_to(bMap)
