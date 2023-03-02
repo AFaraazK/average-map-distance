3aed4fd0586a601cf1a22a59a4748d048b6d677c
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2023-03-01 23:20:10 -0500

    cumulative distance loop

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ for j in range(2):
     ptLat = float(input("Enter the lattitude: "))
     ptLong = float(input("Enter the longitude: "))
     cds.append((ptLat,ptLong))
+
+
+cumulDist = [] # distance between each nC2 points
+
+for index in range(len(cds)):
+    folium.CircleMarker(cds[index]).add_to(bMap)
+    # TODO: add text on screen listing the coords and the avg distance
+    for i in range(len(cds)):
+        #print(cds[index],cds[i])
+        cumulDist.append(geopy.distance.geodesic(cds[index],cds[i]).miles)
+        folium.PolyLine(locations=(cds[index],cds[i]),color="red",opacity=0.2).add_to(bMap)
