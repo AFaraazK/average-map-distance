ed701dddd6b6bbbb97314f7929174e1772561147
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2023-03-01 23:19:08 -0500

    load map and enter coodinates

--- main.py
+++ main.py
@@ -4,5 +4,12 @@
 import folium
 import geopy.distance
 
+# Create template map, centered on US
 bMap = folium.Map(location=[39.8283,-98.5795], zoom_start=5)
 folium.TileLayer('stamenwatercolor').add_to(bMap)
+
+cds = [] # Coordinate array
+for j in range(2):
+    ptLat = float(input("Enter the lattitude: "))
+    ptLong = float(input("Enter the longitude: "))
+    cds.append((ptLat,ptLong))
